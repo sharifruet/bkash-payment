270c9b08d3b04eaafcc7f0b9b48c330bad3b4f5b
bpl <bpl@DESKTOP-FP8DAF1>
2021-01-24 00:29:42 +0600

    dashboard

--- src/main/java/com/i2gether/beximcom/bkashpayment/bkashpayment/dashboard/controller/DashboardController.java
+++ src/main/java/com/i2gether/beximcom/bkashpayment/bkashpayment/dashboard/controller/DashboardController.java
@@ -1,15 +1,21 @@
 package com.i2gether.beximcom.bkashpayment.bkashpayment.dashboard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.i2gether.beximcom.bkashpayment.bkashpayment.webhook.service.PaymentService;
+
 @Controller 
 @RequestMapping("/dashboard")
 public class DashboardController {
+	@Autowired
+	PaymentService paymentService;
 	@GetMapping
 	public String showUserList(Model model) {
+		model.addAttribute("payments", paymentService.getAll());
 	    return "dashboard";
 	}
 }
